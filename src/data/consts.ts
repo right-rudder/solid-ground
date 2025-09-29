071597ec0821dbc8b0a88f31446b1648487e4028
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-09-29 18:06:33 -0300

    Create consts.ts

---
+++ src/data/consts.ts
@@ -0,0 +1,18 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "";
+export const SITE_DESCRIPTION = "";
+export const FACEBOOK_URL = "";
+export const INSTAGRAM_URL = "";
+export const PHONE_NUMBER = "";
+export const EMAIL_ADDRESS = "";
+export const TIKTOK_URL = "";
+export const YOUTUBE_URL = "";
+export const YELP_URL = "";
+export const ADDRESS_LINE_1 = "";
+export const ADDRESS_LINE_2 = "";
+export const GOOGLE_MAPS_URL = "https://maps.app.goo.gl/";
+
+// Keywords for SEO - Comprehensive flight training strategy targeting Salt Lake City and Ogden
+export const SITE_KEYWORDS = "";
